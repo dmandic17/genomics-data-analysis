d02e0f6a5d34130cb8a876205106ccc7c226ce8c
dmandic17 <dejanamandic2@gmail.com>
2020-04-01 19:14:37 +0200

    q2 done, output in the outfile

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import pysam
+import os
 import numpy as np
+import sys
+
+outfile = open('outfile.txt', 'w')
 
 vcf = pysam.VariantFile('experiment_0.vcf')
 samples = np.array(vcf.header.samples)
 
 # Question 1:
-print('\nQuestion 1')
-print('Number of people in the experiment: ', len(samples))
+print('\nQuestion 1', file=outfile)
+print('Number of people in the experiment: {}'.format(len(samples)), file=outfile)
 infected_num = 0
 for sample in samples:
     if sample.startswith('case'):
         infected_num += 1
-print('Number of infected people: ', infected_num)
-print('Number of healthy people: ', len(samples)-infected_num)
+print('Number of infected people: {}'.format(infected_num), file=outfile)
+print('Number of healthy people: {}'.format(len(samples)-infected_num), file=outfile)
 
 # Question 2:
-print('\nQuestion 2')
+print('\nQuestion 2',file=outfile)
+variants = list(vcf.fetch())
+print ('Number of mutations in total:', len(variants), file=outfile)
+chrVariants = {}
+for v in variants:
+    if chrVariants.get(v.chrom) is None:
+        chrVariants[v.chrom] = 0
+    chrVariants[v.chrom]+=1
+for chrm in chrVariants.keys():
+    print('chromosome {} | mutations num {}'.format(chrm, chrVariants[chrm]),file=outfile)
+
+outfile.close()
